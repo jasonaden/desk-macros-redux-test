3847fe61284f65383daa6b6171026f6a5b718dd1
Jason Aden <jasonaden@gmail.com>
2016-02-23 11:57:13 -0800

    Put webpack config in place

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
-module.exports = {  
-  entry: './app/app.ts',
-  output: {
-    filename: 'dist/app.js'
-  },
-  devtool: 'source-map',
+var path = require('path');
+
+module.exports = {
+
+  // set the context (optional)
+  context: path.join( __dirname, '/src'),
+  entry: 'index.js',
+
+  // enable loading modules relatively (without the ../../ prefix)
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    root: path.join( __dirname, '/src')
   },
+
   module: {
     loaders: [
-      { test: /\.ts$/, loader: 'ts-loader' }
+
+      // load and compile javascript
+      // { test: /\.js$/, exclude: /node_modules/, loader:"babel", query: { presets: ['es2015', 'stage-1'] } },
+      { test: /\.ts$/, exclude: /node_modules/, loader:"ts-loader"},
+
+      // load css and process less
+      { test: /\.css$/, loader: "style!css"},
+
+      // load JSON files and HTML
+      { test: /\.json$/, loader: "json" },
+      { test: /\.html$/, exclude: /node_modules/, loader:"raw" },
+
+      // load fonts(inline base64 URLs for <=8k)
+      { test: /\.(ttf|eot|svg|otf)$/, loader: "file" },
+      { test: /\.woff(2)?$/, loader: "url?limit=8192&minetype=application/font-woff"},
+
+      // load images (inline base64 URLs for <=8k images)
+      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
     ]
-  }
-}
+  },
+
+  // webpack dev server configuration
+  devServer: {
+    contentBase: "./src",
+    noInfo: false,
+    hot: true
+  },
+
+  // support source maps
+  devtool: "#inline-source-map"
+};
