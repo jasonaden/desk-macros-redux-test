3cce31da36041e22e64dce3e03174865fb37735e
David Aden <daden@salesforce.com>
2016-08-24 17:39:33 -0400

    use Case.get() instead of a call to the function and remove some old commented out stuff

--- src/modules/desk-agent-case-detail/config/routes.ts
+++ src/modules/desk-agent-case-detail/config/routes.ts
@@ -19,46 +19,13 @@ export const routes = ($stateProvider, $urlRouterProvider, $locationProvider) =>
         if (detail) {
           $ngRedux.dispatch(setActiveCaseId(id));
         } else {
-          // TODO: Convert to using Case.get()
-          const kase = getCaseById($ngRedux.getState(), id);
+          const kase = Case.get(id);
+          // TODO: when users is ready, switch over to User.get()
           const users = getUsers($ngRedux.getState());
 
           return Promise.all( [kase || Case.findOne(id), users.size || User.list()] ).then(()=>{
             setupCaseDetail( Case.get(id) )
           });
-          /*.then(()=>{
-
-            // Note.findOne( id, 1)
-
-            // TODO: commented out original to test populateRelated() function
-            // return setupCaseDetail(getCaseById($ngRedux.getState(), id))
-
-            Case.populateRelatedList(id, 'notes')
-            .then( (data) => {
-              console.log("***** Got the related notes", data);
-            })
-
-            return Case.populateRelated(id, 'customer')
-            .then( (data) => {
-
-              console.log("**** about to synchronously get the related customer", data)
-
-              let tCustomer = Case.getRelated(id, 'customer');
-
-              console.log("**** get Related customer", tCustomer.toJS()) 
-
-              return setupCaseDetail( Case.get(id) )
-            })
-        
-            // TODO: Test populateGetRelated()
-            // return Case.populateGetRelated(id, 'customer')
-            // .then( (data) => {
-            //   console.log("**** get Related customer", data.toJS()) 
-
-            //   // Using uiCase to get the case because we know it will be in the store
-            //   return setupCaseDetail( Case.get(id) )
-            // })
-          });*/
 
         }
         
